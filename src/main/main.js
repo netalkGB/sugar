055436b7a3b817bff82f0a7d1b140b1df3f3e85f
gb <ichikawaorkouki@gmail.com>
2018-09-03 23:19:26 +0900

    logger

--- src/main/main.js
+++ src/main/main.js
@@ -1,14 +1,23 @@
 import { app, BrowserWindow } from 'electron'
+import log4js from 'log4js'
 let mainWindow
 
+const logger = log4js.getLogger('main')
+const level = process.env.NODE_ENV === 'development' ? 'all' : 'warn'
+logger.level = level
+logger.debug('development mode.')
+
 function createWindow () {
-  console.log(process.env.NODE_ENV)
   mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.webContents.openDevTools()
+  }
   mainWindow.loadFile('out/renderer/index.html')
   mainWindow.on('closed', function () {
     mainWindow = null
   })
 }
+
 app.on('ready', createWindow)
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App'
+import log4js from 'log4js'
+
+const logger = log4js.getLogger('renderer')
+logger.level = process.env.NODE_ENV === 'development' ? 'all' : 'warn'
+logger.debug('development mode.')
 
 Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App)
 }).$mount('#app')
