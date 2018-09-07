ec0ad37a86b76d67f8b4c18462b1f618ffd5c731
gb <ichikawaorkouki@gmail.com>
2018-09-08 04:23:13 +0900

    BLACK☆ROCK SHOOTER

---
+++ src/renderer/views/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    userId:{{userId}}
+    <router-link :to="{name:'home_timeline'}">home_timeline</router-link>
+    <router-link :to="{name:'local_timeline'}">local_timeline</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { userId: Number }
+}
+</script>
+
+<style>
+</style>
