6571888a1b57798c6d1d3497ac687533bc2f132b
gb <ichikawaorkouki@gmail.com>
2018-09-10 04:02:16 +0900

    スクロールバー削除&&画面幅取得

--- src/renderer/views/Main.vue
+++ src/renderer/views/Main.vue
@@ -1,17 +1,44 @@
 <template>
-  <div>
-    userId:{{userId}}
-    <router-link :to="{name:'home_timeline'}">home_timeline</router-link>
-    <router-link :to="{name:'local_timeline'}">local_timeline</router-link>
-    <router-view></router-view>
+  <div id="main" ref="main" :style="{ width: width + 'px', height: height + 'px' }">
+    <div id="menu">
+      <router-link :to="{name:'home_timeline'}">htl</router-link>
+      <br>
+      <router-link :to="{name:'local_timeline'}">ltl</router-link>
+    </div>
+    <div id="content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
+import logger from '../other/Logger'
+
 export default {
-  props: { userId: Number }
+  props: { userId: Number },
+  data () {
+    return {
+      width: 0,
+      height: 0
+    }
+  },
+  mounted () {
+    this.width = window.innerWidth
+    this.height = window.innerHeight
+    window.addEventListener('resize', (e) => {
+      this.width = window.innerWidth
+      this.height = window.innerHeight
+    })
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize')
+  }
 }
 </script>
 
-<style>
+<style scoped>
+#main {
+  display: flex;
+  background-color: gainsboro;
+}
 </style>
