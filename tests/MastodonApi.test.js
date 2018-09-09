ef2d3b045cc264b061ecfc0b4059e8e0a6cf3e8a
gb <ichikawaorkouki@gmail.com>
2018-09-10 02:14:23 +0900

    fetch and toot

---
+++ tests/MastodonApi.test.js
@@ -0,0 +1,57 @@
+import Mastodon from '@/main/mastodon/Mastodon'
+import testconfig from './testconfig'
+const { accessToken, host } = testconfig
+
+const mastodon = new Mastodon({ accessToken, host })
+
+describe('fetch toot', async () => {
+  it('should be fetch toot from home', async () => {
+    const result = await mastodon.fetchHomeTimeline()
+    for (let toot of result.data) {
+      expect(toot.content.indexOf('<p>') >= 0).toBe(true)
+    }
+  })
+  it('should be fetch and retrieve toot from ltl', async () => {
+    const result = await mastodon.fetchHomeTimeline()
+    let toots = []
+    for (let toot of result.data) {
+      toots = [...toots, toot.content]
+    }
+    const lastTootIdx = result.data.length - 1
+    const result2 = await mastodon.fetchHomeTimeline({
+      maxID: result.data[lastTootIdx].id
+    })
+    for (let toot of result2.data) {
+      toots = [...toots, toot.content]
+    }
+    expect(toots.length).toBe(40)
+    for (let toot of toots) {
+      expect(toot.indexOf('<p>') >= 0).toBe(true)
+    }
+  })
+
+  it('should be fetch toot from ltl', async () => {
+    const result = await mastodon.fetchLocalTimeline()
+    for (let toot of result.data) {
+      expect(toot.content.indexOf('<p>') >= 0).toBe(true)
+    }
+  })
+  it('should be fetch and retrieve toot from home', async () => {
+    const result = await mastodon.fetchLocalTimeline()
+    let toots = []
+    for (let toot of result.data) {
+      toots = [...toots, toot.content]
+    }
+    const lastTootIdx = result.data.length - 1
+    const result2 = await mastodon.fetchHomeTimeline({
+      maxID: result.data[lastTootIdx].id
+    })
+    for (let toot of result2.data) {
+      toots = [...toots, toot.content]
+    }
+    expect(toots.length).toBe(40)
+    for (let toot of toots) {
+      expect(toot.indexOf('<p>') >= 0).toBe(true)
+    }
+  })
+})
