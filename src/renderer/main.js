bc1acb16ca9a1838f8475857402d19941c542cf0
gb <ichikawaorkouki@gmail.com>
2018-09-16 04:49:32 +0900

    remove buefy

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import App from './App'
-import Buefy from 'buefy'
 import router from './router'
 import store from './store'
 import logger from './other/Logger'
-import '../../node_modules/buefy/lib/buefy.css'
+import 'vue-ionicons/ionicons.css'
 logger.debug('development mode.')
 
 Vue.config.productionTip = false
 
-Vue.use(Buefy)
 new Vue({
   router,
   store,
