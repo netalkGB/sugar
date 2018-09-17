b4b6bf9c61f7223c9a3e8b155ce8a2c43ebe151f
gb <ichikawaorkouki@gmail.com>
2018-09-17 22:46:22 +0900

    仮のメニュー

--- src/renderer/components/Sidebar/SortableMenu.vue
+++ src/renderer/components/Sidebar/SortableMenu.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="sortablemenu">
-    <Icon :to="{name:'home_timeline'}" icon="home_timeline"/>
-    <Icon :to="{name:'local_timeline'}" icon="local_timeline"/>
+    <draggable v-model="menu" @start="drag=true" @end="drag=false">
+      <div v-for="item in menu" :key="item.icon">
+        <Icon :to="item.to" :icon="item.icon"/>
+      </div>
+    </draggable>
   </div>
 </template>
 
 <script>
+import logger from '../../other/Logger'
 import Icon from './Icon'
+import draggable from 'vuedraggable'
 export default {
-  components: { Icon },
-  methods: {}
+  components: { Icon, draggable },
+  data () {
+    return {
+      drag: false,
+      menu: [
+        { icon: 'home_timeline', to: { name: 'home_timeline' } },
+        { icon: 'local_timeline', to: { name: 'local_timeline' } }
+      ]
+    }
+  },
+  methods: {},
+  watch: {
+    menu (newVal) {
+      logger.debug('menu', JSON.stringify(newVal))
+    },
+    drag (newVal) {
+      logger.debug('newVal', newVal)
+    }
+  }
 }
 </script>
 
