b5d1130a90f9a182dc964ab69965dafee1d6805b
gb <ichikawaorkouki@gmail.com>
2018-09-21 19:46:19 +0900

    test

--- tests/TootModel.test.js
+++ tests/TootModel.test.js
@@ -14,4 +14,17 @@ describe('Toot', () => {
     expect(converted.visibility).toBe('unlisted')
     expect(converted.medium !== undefined).toBe(true)
   })
+  it('should be convert correctly(reblog))', () => {
+    const toot = data[2]
+    const converted = Toot.fromMastodon(toot)
+    expect(converted.boostedBy.userid).toBe('netalkGB')
+    expect(converted.profile !== undefined).toBe(true)
+    expect(converted.content).toBe('\u003cp\u003eboost\u003c/p\u003e')
+    expect(converted.date.toString()).toBe(new Date(converted.date).toString())
+    expect(converted.boostsCount).toBe(20)
+    expect(converted.favoritesCount).toBe(7)
+    expect(converted.repliesCount).toBe(1)
+    expect(converted.visibility).toBe('unlisted')
+    expect(converted.medium !== undefined).toBe(true)
+  })
 })
