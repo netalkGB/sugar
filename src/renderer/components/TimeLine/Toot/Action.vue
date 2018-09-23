d96eee9b23235a1564c39c448cd99b642dad2bf8
gb <ichikawaorkouki@gmail.com>
2018-09-24 04:36:38 +0900

    ふぁぼとブーストの実行に対応

--- src/renderer/components/TimeLine/Toot/Action.vue
+++ src/renderer/components/TimeLine/Toot/Action.vue
@@ -2,15 +2,33 @@
   <div id="action">
     <div id="reply"><IosUndoIcon :w="'18'" :h="'18'" /> <div class="count">{{repliesCount}}</div></div>
     <template v-if="visibility === 'public' || visibility === 'unlisted'">
-      <div id="boost" ><MdRepeatIcon :w="'18'" :h="'18'" /><div class="count">{{boostsCount}}</div></div>
+      <div id="boost" :class="{boosted:displayBoosted}" @click="boostToot">
+        <MdRepeatIcon :w="'18'" :h="'18'" />
+        <div class="count">
+          <span v-if="displayBoostsCount > 0">
+            {{displayBoostsCount}}
+          </span>
+        </div>
+      </div>
     </template>
     <template v-else-if="visibility === 'private'">
-      <div id="protectedboost"><IosLockIcon :w="'18'" :h="'18'" /><div class="count"></div></div>
+      <div id="protectedboost">
+        <IosLockIcon :w="'18'" :h="'18'" />
+        <div class="count"></div>
+      </div>
     </template>
     <template v-else>
-      <div id="dmboost"><IosMailIcon :w="'18'" :h="'18'" /><div class="count"></div></div>
+      <div id="dmboost">
+        <IosMailIcon :w="'18'" :h="'18'" />
+        <div class="count"></div>
+      </div>
     </template>
-    <div id="favorite"> <IosStarIcon :w="'18'" :h="'18'" /><div class="count">{{favoritesCount}}</div></div>
+    <div id="favorite" :class="{favorited:displayFavorited}" @click="favoriteToot">
+      <IosStarIcon :w="'18'" :h="'18'" />
+      <div class="count">
+        <span v-if="displayFavoritesCount > 0">{{displayFavoritesCount}}</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,10 +38,67 @@ import MdRepeatIcon from 'vue-ionicons/dist/md-repeat.vue'
 import IosStarIcon from 'vue-ionicons/dist/ios-star.vue'
 import IosLockIcon from 'vue-ionicons/dist/ios-lock.vue'
 import IosMailIcon from 'vue-ionicons/dist/ios-mail.vue'
+import { mapActions } from 'vuex'
+import logger from '../../../other/Logger'
 
 export default {
   components: { IosUndoIcon, MdRepeatIcon, IosStarIcon, IosLockIcon, IosMailIcon },
-  props: ['visibility', 'boostsCount', 'favoritesCount', 'repliesCount']
+  props: ['visibility', 'boostsCount', 'favoritesCount', 'repliesCount', 'favorited', 'boosted', 'id'],
+  data () {
+    return {
+      displayBoosted: false,
+      displayFavorited: false,
+      displayFavoritesCount: 0,
+      displayBoostsCount: 0
+    }
+  },
+  created () {
+    this.displayBoosted = this.boosted
+    this.displayFavorited = this.favorited
+    this.displayFavoritesCount = this.favoritesCount
+    this.displayBoostsCount = this.boostsCount
+  },
+  methods: {
+    favoriteToot () {
+      if (this.displayFavorited) {
+        logger.debug('to unfavorite')
+        this.displayFavorited = false
+        this.displayFavoritesCount--
+        this.unFavorite({ id: this.id }).catch((e) => {
+          this.displayFavorited = true
+          this.displayFavoritesCount++
+        })
+      } else {
+        logger.debug('to favorite')
+        this.displayFavoritesCount++
+        this.displayFavorited = true
+        this.favorite({ id: this.id }).catch((e) => {
+          this.displayFavorited = false
+          this.displayFavoritesCount--
+        })
+      }
+    },
+    boostToot () {
+      if (this.displayBoosted) {
+        logger.debug('to unboost')
+        this.displayBoosted = false
+        this.displayBoostsCount--
+        this.unBoost({ id: this.id }).catch((e) => {
+          this.displayBoosted = true
+          this.displayBoostsCount++
+        })
+      } else {
+        logger.debug('to boost')
+        this.displayBoostsCount++
+        this.displayBoosted = true
+        this.boost({ id: this.id }).catch((e) => {
+          this.displayBoosted = false
+          this.displayBoostsCount--
+        })
+      }
+    },
+    ...mapActions('timelines', ['favorite', 'unFavorite', 'boost', 'unBoost'])
+  }
 }
 </script>
 
--- tests/TootModel.test.js
+++ tests/TootModel.test.js
@@ -7,6 +7,8 @@ describe('Toot', () => {
     const converted = Toot.fromMastodon(toot)
     expect(converted.profile !== undefined).toBe(true)
     expect(converted.content).toBe('\u003cp\u003emastodon\u003c/p\u003e')
+    expect(converted.favorited).toBe(false)
+    expect(converted.boosted).toBe(false)
     expect(converted.date.toString()).toBe(new Date(converted.date).toString())
     expect(converted.boostsCount).toBe(0)
     expect(converted.favoritesCount).toBe(0)
@@ -18,6 +20,8 @@ describe('Toot', () => {
   it('should be convert correctly(reblog))', () => {
     const toot = data[2]
     const converted = Toot.fromMastodon(toot)
+    expect(converted.favorited).toBe(true)
+    expect(converted.boosted).toBe(true)
     expect(converted.boostedBy.userid).toBe('netalkGB')
     expect(converted.profile !== undefined).toBe(true)
     expect(converted.content).toBe('\u003cp\u003eboost\u003c/p\u003e')
