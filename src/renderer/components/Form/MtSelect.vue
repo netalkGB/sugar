db98bc7521101f7ac6aaa40c3172d846fbc39342
gb <ichikawaorkouki@gmail.com>
2018-10-12 00:11:08 +0900

    ボタン等ののスタイルを変更

---
+++ src/renderer/components/Form/MtSelect.vue
@@ -0,0 +1,36 @@
+<template>
+  <select :disabled="disabled" class="select">
+    <slot></slot>
+  </select>
+</template>
+
+<script>
+export default {
+  props: {
+    disabled: {
+      type: Boolean,
+      defualt: false,
+      required: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.select {
+  border: 1px solid #bbbbbb;
+  background-color: #fefefe;
+  padding: 3px 4px;
+  border-radius: 2px;
+  outline: none;
+}
+.select:disabled {
+  background-color: white;
+}
+.select:hover {
+  box-shadow: 0 0px 3px rgba(0, 0, 0, 0.3);
+}
+.select:disabled:hover {
+  box-shadow: none;
+}
+</style>
