adacd0d14678b365512a2a6e9b99607f64777d27
gb <ichikawaorkouki@gmail.com>
2018-10-15 01:10:47 +0900

    refactor

---
+++ src/renderer/other/File.js
@@ -0,0 +1,25 @@
+import uuidv4 from 'uuid/v4'
+export default class File {
+  constructor ({ filePath, uuid, id, state }) {
+    this.filePath = filePath
+    this.uuid = uuid
+    if (id === undefined) {
+      this.id = null
+    } else {
+      this.id = id
+    }
+    this.state = state
+  }
+  static setFile ({ filePath, state }) {
+    const uuid = uuidv4()
+    return new File({ filePath, uuid, state })
+  }
+  changeState ({ newState, id }) {
+    return new File({
+      filePath: this.filePath,
+      uuid: this.uuid,
+      id,
+      state: newState
+    })
+  }
+}
