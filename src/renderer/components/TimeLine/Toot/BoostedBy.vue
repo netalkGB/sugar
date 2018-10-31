0049f84b522e518eeb31b095e3b16f6773b82fc1
gb <ichikawaorkouki@gmail.com>
2018-11-01 01:33:00 +0900

    名前を設定していないと空になる不具合を修正

--- src/renderer/components/TimeLine/Toot/BoostedBy.vue
+++ src/renderer/components/TimeLine/Toot/BoostedBy.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
-    <MdRepeatIcon :w="'12'" :h="'12'" class="boosted" />{{boostedBy.displayName}}さんがブースト
+    <MdRepeatIcon :w="'12'" :h="'12'" class="boosted" />
+    <NotificationName :profile="boostedBy" />さんがブースト
   </div>
 </template>
 
 <script>
 import MdRepeatIcon from 'vue-ionicons/dist/md-repeat.vue'
+import NotificationName from './NotificationName'
 
 export default {
-  components: { MdRepeatIcon },
+  components: { MdRepeatIcon, NotificationName },
   props: ['boostedBy']
 }
 </script>
