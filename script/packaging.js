4703064fd3d823bd175e002b7ee88c202ca8018b
gb <ichikawaorkouki@gmail.com>
2018-11-03 23:38:00 +0900

    asynchronousに

--- script/packaging.js
+++ script/packaging.js
@@ -1,5 +1,6 @@
 const packager = require('electron-packager')
 const fs = require('fs')
+const del = require('del')
 const { buildAll } = require('./webpack')
 
 const packagejson = require(`${__dirname}/../package.json`)
@@ -7,19 +8,38 @@ const packagejson = require(`${__dirname}/../package.json`)
 const platform = process.argv[2]
 const arch = process.argv[3]
 
-async function main () {
+const writeFilePromise = (path, data) =>
+  new Promise((resolve, reject) => {
+    fs.writeFile(path, data, err => {
+      if (err) {
+        reject(err)
+      }
+      resolve()
+    })
+  })
+
+const main = async () => {
+  await del(['./dist', './prodOut'])
   if ((await buildAll()) === false) {
     process.exit(1)
   }
   const { name, version, description, license } = packagejson
-  const prodPackagejson = {
-    name,
-    version,
-    main: 'out/main/main.js',
-    description,
-    license
+  try {
+    const prodPackagejson = {
+      name,
+      version,
+      main: 'out/main/main.js',
+      description,
+      license
+    }
+    await writeFilePromise(
+      './prodOut/package.json',
+      JSON.stringify(prodPackagejson)
+    )
+  } catch (e) {
+    console.error(e)
+    process.exit(1)
   }
-  fs.writeFileSync('./prodOut/package.json', JSON.stringify(prodPackagejson))
   try {
     await packager({
       arch,
