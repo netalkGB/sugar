a9baf6590a37b6c48432dafbee1f434b58605233
gb <ichikawaorkouki@gmail.com>
2018-11-03 23:38:48 +0900

    format

--- script/build.js
+++ script/build.js
@@ -1,6 +1,6 @@
 const { buildAll, rendererProcess, mainProcess } = require('./webpack')
 
-async function main() {
+async function main () {
   const mode = process.argv[2]
   const target = process.argv[3]
   if (target === 'renderer') {
@@ -8,7 +8,7 @@ async function main() {
   } else if (target === 'main') {
     process.exit((await mainProcess({ mode })) === true ? 0 : 1)
   } else {
-    process.exit(await buildAll(mode) ? 0 : 1)
+    process.exit((await buildAll(mode)) ? 0 : 1)
   }
 }
 
