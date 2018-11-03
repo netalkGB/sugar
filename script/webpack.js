ad81b24f7c6ec764a4fb31d98d9d174767fd22f6
gb <ichikawaorkouki@gmail.com>
2018-11-03 23:38:48 +0900

    format

--- script/webpack.js
+++ script/webpack.js
@@ -8,74 +8,73 @@ const green = '\u001b[32m'
 const red = '\u001b[31m'
 const yellow = '\u001b[33m'
 
-
 const rendererProcess = ({ mode }) =>
-    new Promise(resolve => {
-        webpack(rendererConfig({}, { mode }), (err, stats) => {
-            const r = printLog('renderer', err, stats)
-            resolve(r)
-        })
+  new Promise(resolve => {
+    webpack(rendererConfig({}, { mode }), (err, stats) => {
+      const r = printLog('renderer', err, stats)
+      resolve(r)
     })
+  })
 
 const mainProcess = ({ mode }) =>
-    new Promise(resolve => {
-        webpack(mainConfig({}, { mode }), (err, stats) => {
-            const r = printLog('main', err, stats)
-            resolve(r)
-        })
+  new Promise(resolve => {
+    webpack(mainConfig({}, { mode }), (err, stats) => {
+      const r = printLog('main', err, stats)
+      resolve(r)
     })
+  })
 
 const buildAll = async mode => {
-    const [renderer, main] = await Promise.all([
-        rendererProcess({ mode }),
-        mainProcess({ mode })
-    ])
+  const [renderer, main] = await Promise.all([
+    rendererProcess({ mode }),
+    mainProcess({ mode })
+  ])
 
-
-    const mainResult =
-        (main === true ? green + 'success 🙆' : red + 'failed 🙅') + reset
-    const rendererResult =
-        (renderer === true ? green + 'success 🙆' : red + 'failed 🙅') + reset
-    console.log('main:     ' + mainResult)
-    console.log('renderer: ' + rendererResult)
-    return main === true && renderer === true
+  const mainResult =
+    (main === true ? green + 'success 🙆' : red + 'failed 🙅') + reset
+  const rendererResult =
+    (renderer === true ? green + 'success 🙆' : red + 'failed 🙅') + reset
+  console.log('main:     ' + mainResult)
+  console.log('renderer: ' + rendererResult)
+  return main === true && renderer === true
 }
 
 const watch = ({ mode }) => {
-    const mainCompiler = webpack(mainConfig({}, { mode }))
-    const rendererCompiler = webpack(rendererConfig({}, { mode }))
+  const mainCompiler = webpack(mainConfig({}, { mode }))
+  const rendererCompiler = webpack(rendererConfig({}, { mode }))
 
-    console.log(yellow + 'initial building...' + reset)
+  console.log(yellow + 'initial building...' + reset)
 
-    let mainResult = yellow + 'initial building...' + reset
-    let rendererResult = yellow + 'initial building...' + reset
+  let mainResult = yellow + 'initial building...' + reset
+  let rendererResult = yellow + 'initial building...' + reset
 
-    mainCompiler.watch(
-        {
-            aggregateTimeout: 300,
-            poll: 150
-        },
-        (err, stats) => {
-            const r = printLog('renderer', err, stats)
-            mainResult = (r === true ? green + 'success 🙆' : red + 'failed 🙅') + reset
-            console.log('main:     ' + mainResult)
-            console.log('renderer: ' + rendererResult)
-        }
-    )
+  mainCompiler.watch(
+    {
+      aggregateTimeout: 300,
+      poll: 150
+    },
+    (err, stats) => {
+      const r = printLog('renderer', err, stats)
+      mainResult =
+        (r === true ? green + 'success 🙆' : red + 'failed 🙅') + reset
+      console.log('main:     ' + mainResult)
+      console.log('renderer: ' + rendererResult)
+    }
+  )
 
-    rendererCompiler.watch(
-        {
-            aggregateTimeout: 300,
-            poll: 150
-        },
-        (err, stats) => {
-            const r = printLog('main', err, stats)
-            rendererResult = (r === true ? green + 'success 🙆' : red + 'failed 🙅') + reset
-            console.log('main:     ' + mainResult)
-            console.log('renderer: ' + rendererResult)
-        }
-    )
-
+  rendererCompiler.watch(
+    {
+      aggregateTimeout: 300,
+      poll: 150
+    },
+    (err, stats) => {
+      const r = printLog('main', err, stats)
+      rendererResult =
+        (r === true ? green + 'success 🙆' : red + 'failed 🙅') + reset
+      console.log('main:     ' + mainResult)
+      console.log('renderer: ' + rendererResult)
+    }
+  )
 }
 
 exports.buildAll = buildAll
