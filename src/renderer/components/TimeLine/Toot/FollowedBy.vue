4e4cdd78def6bd3889f6117d19dc8a3a29a72e0c
gb <ichikawaorkouki@gmail.com>
2018-11-22 06:05:48 +0900

    通知アイコンを上下中央に

--- src/renderer/components/TimeLine/Toot/FollowedBy.vue
+++ src/renderer/components/TimeLine/Toot/FollowedBy.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <MdPersonAddIcon :w="'12'" :h="'12'" class="followedBy" />フォローされました
+  <div class="container">
+    <div class="icon">
+      <MdPersonAddIcon :w="'12'" :h="'12'" class="followedBy" />
+    </div>
+    <div class="message">
+      フォローされました
+    </div>
   </div>
 </template>
 
@@ -13,8 +18,25 @@ export default {
 }
 </script>
 
-<style scope>
+<style scoped>
+:root {
+  --icon-size: 12px;
+}
+.container {
+  display: flex;
+}
+.icon {
+  width: var(--icon-size);
+  height: 15px;
+  display: flex;
+  align-items: center;
+}
+.message {
+  width: calc(100% - var(--icon-size));
+}
+
 .followedBy {
   color: #0000ff;
 }
+
 </style>
