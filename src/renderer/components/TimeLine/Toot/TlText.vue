e8f8d42d35139dd89e7b2eb0db77024955bebe0f
gb <ichikawaorkouki@gmail.com>
2018-11-23 22:49:43 +0900

    リンクを正しく機能させる

--- src/renderer/components/TimeLine/Toot/TlText.vue
+++ src/renderer/components/TimeLine/Toot/TlText.vue
@@ -4,24 +4,67 @@
 
 <script>
 import { shell } from 'electron'
+import logger from '@/other/Logger'
 export default {
   props: ['content'],
   data () {
     return {
-      hrefs: null
+      users: null,
+      hashtags: null,
+      links: null,
+      atag: null
     }
   },
   mounted () {
-    this.hrefs = this.$refs.tltext.querySelectorAll('p > a')
-    for (let a of this.hrefs) {
-      a.addEventListener('click', (e) => {
+    const tltext = this.$refs.tltext
+    this.atag = tltext.querySelectorAll('a')
+    for (let a of this.atag) {
+      a.addEventListener('click', e => {
         e.preventDefault()
-        shell.openExternal(e.path.find(elem => elem.href).href)
+      })
+    }
+    this.users = tltext.querySelectorAll('a.u-url')
+    for (let user of this.users) {
+      user.addEventListener('click', e => {
+        e.preventDefault()
+        const href = user.href
+        const split = href.split('/')
+        logger.debug('user', split[2], split[3], href)
+      })
+    }
+    this.hashtags = tltext.querySelectorAll('a.hashtag')
+    for (let hashtag of this.hashtags) {
+      hashtag.addEventListener('click', e => {
+        e.preventDefault()
+        const href = hashtag.href
+        const tag = hashtag.querySelector('a > span').textContent
+        logger.debug(href)
+        logger.debug(tag)
+      })
+    }
+    this.links = tltext.querySelectorAll('a[target="_blank"]')
+    for (let link of this.links) {
+      link.addEventListener('click', e => {
+        e.preventDefault()
+        if (link.textContent && link.textContent.split('')[0] !== '@') {
+          const href = link.href
+          logger.debug(href)
+          shell.openExternal(href)
+        }
       })
     }
   },
   beforeDestroy () {
-    for (let a of this.hrefs) {
+    for (let a of this.users) {
+      a.removeEventListener('click', (e) => { })
+    }
+    for (let a of this.hashtags) {
+      a.removeEventListener('click', (e) => { })
+    }
+    for (let a of this.links) {
+      a.removeEventListener('click', (e) => { })
+    }
+    for (let a of this.atag) {
       a.removeEventListener('click', (e) => { })
     }
   }
