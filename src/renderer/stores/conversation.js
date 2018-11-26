e6a4e9c1fd861075829bd2a8eb6da0826be9420e
gb <ichikawaorkouki@gmail.com>
2018-11-27 04:55:36 +0900

    ウインドウを飛び越えて削除

--- src/renderer/stores/conversation.js
+++ src/renderer/stores/conversation.js
@@ -40,21 +40,38 @@ export default {
     setConversations (state, payload) {
       const { conversations } = payload
       state.conversations = conversations
+    },
+    removeTootFromTl (state, payload) {
+      const { id } = payload
+      const roundedId = parseInt(id, 10)
+      console.log(state.conversations)
+      state.conversations = state.conversations.filter(
+        toot => parseInt(toot.id) !== roundedId
+      )
     }
   },
   actions: {
+    removeToot ({ commit }, payload) {
+      const id = payload.id
+      commit('removeTootFromTl', { id })
+    },
     async loadConversations ({ commit }, { tootId }) {
-      const { accessToken, host } = this.getters['users/getCurrentUser']
+      const { accessToken, host, user } = this.getters['users/getCurrentUser']
       logger.debug(accessToken)
       logger.debug(host)
+
       const [toot, context] = await Promise.all([
         loadToot({ accessToken, host, id: tootId }),
         loadContext({ accessToken, host, id: tootId })
       ])
       let data = [
-        ...context.result.data.ancestors.map(item => Toot.fromMastodon(item)),
-        Toot.fromMastodon(toot.result.data),
-        ...context.result.data.descendants.map(item => Toot.fromMastodon(item))
+        ...context.result.data.ancestors.map(item =>
+          Toot.fromMastodon(item, user)
+        ),
+        Toot.fromMastodon(toot.result.data, user),
+        ...context.result.data.descendants.map(item =>
+          Toot.fromMastodon(item, user)
+        )
       ]
       logger.debug(data.length, 'item(s)')
       commit('setConversations', { conversations: data })
