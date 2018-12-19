e48e2d2da896aeb808561a561bbe7f8af5fbd0e9
gb <ichikawaorkouki@gmail.com>
2018-12-20 02:30:04 +0900

    フォロー数など追加

--- src/renderer/other/Profile.js
+++ src/renderer/other/Profile.js
@@ -5,6 +5,10 @@ export default class Profile {
     this.userid = args.userid
     this.locked = args.locked
     this.note = args.note
+    this.internalid = args.internalid
+    this.followersCount = args.followersCount
+    this.followingCount = args.followingCount
+    this.statusesCount = args.statusesCount
   }
   static fromAccount (account) {
     const avatar = account.avatar
@@ -12,6 +16,20 @@ export default class Profile {
     const userid = account.acct
     const locked = account.locked
     const note = account.note
-    return new Profile({ avatar, displayName, userid, locked, note })
+    const internalid = account.id
+    const followersCount = account.followers_count
+    const followingCount = account.following_count
+    const statusesCount = account.statuses_count
+    return new Profile({
+      avatar,
+      displayName,
+      userid,
+      locked,
+      note,
+      internalid,
+      followersCount,
+      followingCount,
+      statusesCount
+    })
   }
 }
