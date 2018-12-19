08b874205839aa4c626e61cf06b81c6460021307
gb <ichikawaorkouki@gmail.com>
2018-12-20 02:42:50 +0900

    とりあえずコンポーネントとして分割

--- src/renderer/views/Profile.vue
+++ src/renderer/views/Profile.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>{{userId}}/{{internalId}}</div>
+  <Profile
+    :userId="userId"
+    :internalId="internalId"
+  />
 </template>
 
 <script>
+import Profile from '@/components/Profile/Profile'
 export default {
-  props: ['userId', 'internalId']
+  props: ['userId', 'internalId'],
+  components: { Profile }
 }
 </script>
 
