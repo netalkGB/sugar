14abf99adda697a5f07620c2138efa5a10f999c2
gb <ichikawaorkouki@gmail.com>
2018-12-22 23:51:04 +0900

    プロフィールの表示

--- src/renderer/components/Profile/Profile.vue
+++ src/renderer/components/Profile/Profile.vue
@@ -1,14 +1,18 @@
 <template>
-  <div style="word-wrap: break-word;">
-    [WIP]<br>{{profile}}<br>{{timeline}}
+  <div class="profile">
+    <Header :profile="profile"/>
   </div>
 </template>
 
 <script>
+import Header from './Header'
 import { createNamespacedHelpers } from 'vuex'
 const { mapActions, mapState } = createNamespacedHelpers('profile')
 
 export default {
+  components: {
+    Header
+  },
   props: ['internalId', 'userId'],
   methods: {
     ...mapActions(['fetchProfile', 'fetchProfileTimeline']),
