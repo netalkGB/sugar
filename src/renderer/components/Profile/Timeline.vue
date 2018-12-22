81be504cc27befd3c5e578e1f5be21e49e1dfa21
gb <ichikawaorkouki@gmail.com>
2018-12-22 23:59:03 +0900

    プロフィールのタイムライン

---
+++ src/renderer/components/Profile/Timeline.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="timeline">
+    <TimeLine :infiniteMode="false" :timeline="timeline" />
+  </div>
+</template>
+
+<script>
+import TimeLine from '@/components/TimeLine/TimeLine'
+export default {
+  props: ['timeline'],
+  components: { TimeLine }
+}
+</script>
+
+<style scoped>
+</style>
