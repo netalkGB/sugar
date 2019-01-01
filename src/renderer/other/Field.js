602c4bc2ed5e4578d21c01adedc9659da5100344
gb <ichikawaorkouki@gmail.com>
2019-01-02 01:12:38 +0900

    add fields

---
+++ src/renderer/other/Field.js
@@ -0,0 +1,12 @@
+export default class Field {
+  constructor (name, value) {
+    this.name = name
+    this.value = value
+  }
+  static fromMastodonField (field) {
+    return new Field(field.name, field.value)
+  }
+  static fromMastodonFields (fields) {
+    return fields.map(f => Field.fromMastodonField(f))
+  }
+}
