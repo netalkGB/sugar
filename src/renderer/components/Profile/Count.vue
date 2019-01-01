8f150a87a9db1d000a4b004f78c96deb430c17a0
gb <ichikawaorkouki@gmail.com>
2019-01-02 02:16:49 +0900

    分割

---
+++ src/renderer/components/Profile/Count.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="countContainer">
+    <div
+      class="status"
+      @click="$emit('toggle')"
+    >
+      <div class="title">トゥート</div>
+      <div class="count">{{toot}}</div>
+    </div>
+    <div class="following">
+      <div class="title">フォロー</div>
+      <div class="count">{{following}}</div>
+    </div>
+    <div class="followers">
+      <div class="title">フォロワー</div>
+      <div class="count">{{follower}}</div>
+    </div>
+    <div class="other"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['follower', 'following', 'toot']
+}
+</script>
+
+<style scoped>
+.countContainer {
+  display: grid;
+  grid-template:
+    "statuse following follower other" 30px
+    / 70px 70px 70px 1fr;
+  padding-bottom: 2px;
+  padding-top: 2px;
+}
+.count {
+  align-items: center;
+  padding-left: 2px;
+}
+.title {
+  font-size: 70%;
+}
+.status {
+  cursor: pointer;
+}
+</style>
