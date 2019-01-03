782997420ad64cfc4323d786149acbc5cb8a45bf
gb <ichikawaorkouki@gmail.com>
2019-01-04 04:03:12 +0900

    ユーザ(リスト)コンポーネント追加

--- src/renderer/components/Profile/Timeline.vue
+++ src/renderer/components/Profile/Timeline.vue
@@ -8,20 +8,29 @@
       />
     </template>
     <template v-if="active === 'following'">
-      <div>{{following.length}}<br>{{following}}</div>
+      <UserList
+        :infiniteMode="false"
+        :users="following"
+      />
+      <!-- <div>{{following.length}}<br>{{following}}</div> -->
     </template>
     <template v-if="active === 'followers'">
-      <div>{{followers.length}}<br>{{followers}}</div>
+      <UserList
+        :infiniteMode="false"
+        :users="followers"
+      />
+      <!-- <div>{{followers.length}}<br>{{followers}}</div> -->
     </template>
   </div>
 </template>
 
 <script>
 import TimeLine from '@/components/TimeLine/TimeLine'
+import UserList from '@/components/TimeLine/UserList'
 
 export default {
   props: ['timeline', 'active', 'followers', 'following'],
-  components: { TimeLine },
+  components: { TimeLine, UserList },
   methods: {
     wantOldToot (args) {
       const { maxID } = args
---
+++ src/renderer/components/TimeLine/UserList.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="userListContainer" @scroll="scrolling" ref="outerList">
+    <div ref="list">
+      <User v-for="(user) in users" :user="user" :key="user.id" />
+      <div v-if="loadingPrev === true" class="loading">
+        <IosSyncIcon w="20px" h="20px" animate="rotate" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import User from '@/components/TimeLine/Toot/User'
+import IosSyncIcon from 'vue-ionicons/dist/ios-sync.vue'
+export default {
+  components: { User, IosSyncIcon },
+  props: {
+    users: {
+      type: Array,
+      required: true
+    },
+    infiniteMode: {
+      type: Boolean,
+      required: false,
+      default: true
+    }
+  },
+  data () {
+    return {
+      scrollState: 'loading(next)',
+      loadingPrev: false,
+      prevScrollHeight: 0
+    }
+  },
+  methods: {
+    scrolling (e) {
+      const { scrollTop, offsetHeight, scrollHeight } = e.target
+      const loadTopHeight = scrollHeight - offsetHeight
+      if (scrollTop <= 0) {
+        this.scrollState = 'loading(next)'
+      } else if (scrollTop >= loadTopHeight) {
+        this.scrollState = 'loading(prev)'
+      } else {
+        this.scrollState = 'buffering'
+      }
+    }
+  },
+  watch: {
+    scrollState: function () {
+      if (this.infiniteMode === false) {
+        return
+      }
+      switch (this.scrollState) {
+        case 'loading(next)':
+          break
+        case 'loading(prev)':
+          if (this.loadingPrev === false) {
+            this.loadingPrev = true
+            this.$emit('wantOldUser', { maxID: this.user[this.user.length - 1].id })
+          }
+          break
+        case 'buffering':
+          this.prevScrollHeight = this.$refs.list.scrollHeight
+          break
+      }
+      this.$emit('scrollStateChanged', this.scrollState)
+    }
+  },
+  created () {
+    this.$on('loadOldUserDone', function (result) {
+      this.loadingPrev = false
+    })
+  },
+  beforeDestroy () {
+    this.$off('loadOldUserDone')
+  }
+}
+</script>
+
+<style scoped>
+.userListContainer {
+  font-size: 12px;
+  height: 100%;
+  width: 100%;
+  overflow-y: scroll;
+  -webkit-overflow-scrolling: touch;
+}
+.loading {
+  text-align: center;
+  color: #00aaff;
+}
+</style>
