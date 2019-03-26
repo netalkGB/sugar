eb4ef2d12db2a23e3065fc71f9129867490d1bf0
gb <ichikawaorkouki@gmail.com>
2019-03-26 23:04:59 +0900

    倍率指定で画像拡大

--- src/renderer/components/ImagePreview/Preview.vue
+++ src/renderer/components/ImagePreview/Preview.vue
@@ -1,27 +1,39 @@
 <template>
-  <div class="preview">
+  <div :class="{center: zoom === 1.0}">
     <img
       class="image"
       :src="url"
       draggable="false"
       alt=""
-      :style="{ 'height': height + 'px' }"
+      :style="{ 'height': autoHeight, 'width': autoWidth, 'zoom':zoom }"
     >
   </div>
 </template>
 
 <script>
 export default {
-  props: ['url', 'height']
+  props: ['url', 'height'],
+  data () {
+    return {
+      zoom: 1
+    }
+  },
+  computed: {
+    autoHeight () {
+      return this.zoom === 1.0 ? this.height + 'px' : 'auto'
+    },
+    autoWidth () {
+      return this.zoom === 1.0 ? '100%' : 'auto'
+    }
+  }
 }
 </script>
 
 <style scoped>
 .image {
-  width: 100%;
   object-fit: contain;
 }
-.preview {
+.center {
   display: flex;
   justify-content: center;
   align-items: center;
