aeca4eaf94ccb8b32cf958b4d745292c2528685d
gb <ichikawaorkouki@gmail.com>
2019-04-13 02:39:33 +0900

    仮の画面

--- src/renderer/views/Search.vue
+++ src/renderer/views/Search.vue
@@ -1,13 +1,36 @@
 <template>
-  <div>aaaa</div>
+  <div><input v-model="query"><input
+      type="button"
+      @click="search"
+    >{{userId}}<br>{{accounts}}<br>{{timeline}}</div>
 </template>
 
 <script>
+import { mapActions, createNamespacedHelpers } from 'vuex'
+const { mapState } = createNamespacedHelpers('search')
 export default {
-
+  props: ['userId'],
+  methods: {
+    ...mapActions('users', ['setCurrentUserId', 'loadUserConfig']),
+    ...mapActions('search', ['searchMastodon']),
+    search () {
+      this.searchMastodon({ q: this.query })
+    }
+  },
+  data () {
+    return {
+      query: ''
+    }
+  },
+  mounted () {
+    this.loadUserConfig()
+    this.setCurrentUserId(this.userId)
+  },
+  computed: {
+    ...mapState({ accounts: state => state.accounts, timeline: state => state.timeline })
+  }
 }
 </script>
 
 <style scoped>
-
 </style>
