8b391a1240baf96d995c4492c7f435871a54b160
gb <ichikawaorkouki@gmail.com>
2019-04-14 01:49:52 +0900

    下をユーザ一覧に

--- src/renderer/components/Search/Search.vue
+++ src/renderer/components/Search/Search.vue
@@ -1,20 +1,27 @@
 <template>
   <div>
-    <SearchBox style="height: 30px"/>
-    <div style="height: calc(100% - 30px);overflow:scroll;">
+    <SearchBox style="height: 30px" />
+    <!-- <div style="height: calc(100% - 30px);">
       {{accounts}}<br>{{timeline}}
-    </div>
+    </div> -->
+    <UserList
+      style="height: calc(100% - 30px);"
+      :infiniteMode="false"
+      :users="accounts"
+    />
   </div>
 </template>
 
 <script>
 import { createNamespacedHelpers } from 'vuex'
 import SearchBox from '@/components/Search/SearchBox'
+import UserList from '@/components/TimeLine/UserList'
 const { mapState } = createNamespacedHelpers('search')
 
 export default {
   components: {
-    SearchBox
+    SearchBox,
+    UserList
   },
   computed: {
     ...mapState({ accounts: state => state.accounts, timeline: state => state.timeline, active: state => state.active })
