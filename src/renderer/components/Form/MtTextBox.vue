695f380c1568d40e1b82295c214099a8f8c5b4bf
gb <ichikawaorkouki@gmail.com>
2019-04-14 23:24:09 +0900

    テキストボックスにxボタンを追加

--- src/renderer/components/Form/MtTextBox.vue
+++ src/renderer/components/Form/MtTextBox.vue
@@ -60,7 +60,7 @@ export default {
     }
   },
   methods: {
-    setVal (value) {  
+    setVal (value) {
       this.val = value
     },
     focus () {
@@ -109,7 +109,7 @@ export default {
 .textBoxWrapper {
   display: inline-flex;
   border: 1px solid #bbbbbb;
-  width: 150px;
+  /* width: 150px; */
 }
 .textBoxWrapper:hover {
   box-shadow: 0 0px 3px rgba(0, 0, 0, 0.3);
