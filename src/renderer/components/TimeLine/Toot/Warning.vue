3a52c185ab20cab59c11fd52442f320b7880e87d
gb <ichikawaorkouki@gmail.com>
2019-04-20 21:31:53 +0900

    CWな場合は投稿を隠して警告を表示しクリックしたら投稿を表示

---
+++ src/renderer/components/TimeLine/Toot/Warning.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="warning">
+    !!!&nbsp;閲覧注意{{comment}}&nbsp;!!!
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    reason: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    comment: function () {
+      return this.reason.length > 0 ? `(${this.reason})` : ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.warning {
+  cursor: pointer;
+}
+</style>
