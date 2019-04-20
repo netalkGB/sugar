c70bf6b5a281bf7a5afe906ef8f6f6797136d609
gb <ichikawaorkouki@gmail.com>
2019-04-21 01:27:55 +0900

    一括方式に変更

--- src/renderer/stores/favourite.js
+++ src/renderer/stores/favourite.js
@@ -1,6 +1,6 @@
 import Toot from '@/other/Toot'
 const ipcRenderer = window.ipc
-
+const limit = 100
 export default {
   namespaced: true,
   state: {
@@ -9,10 +9,6 @@ export default {
   mutations: {
     setTimeline (state, timeline) {
       state.timeline = timeline
-    },
-    appendTimeline (state, payload) {
-      const { timeline } = payload
-      state.timeline = [...state.timeline, ...timeline]
     },
     removeTootFromTl (state, payload) {
       const { id } = payload
@@ -32,35 +28,15 @@ export default {
       const { accessToken, host } = currentUser
       const ownUser = currentUser.user
       return new Promise((resolve, reject) => {
-        ipcRenderer.once('fetchOwnFavourite-success', (_, data) => {
+        ipcRenderer.once('fetchOwnFavouriteTimeline-success', (_, data) => {
           const statuses = data.result.data
           commit('setTimeline', statuses.map(status => Toot.fromMastodon(status, ownUser)))
           resolve()
         })
-        ipcRenderer.once('fetchOwnFavourite-error', (_, e) => {
+        ipcRenderer.once('fetchOwnFavouriteTimeline-error', (_, e) => {
           reject(e)
         })
-        ipcRenderer.send('fetchOwnFavourite', { host, accessToken })
-      })
-    },
-    async loadOldToot ({ commit }, { internalId, maxID }) {
-      const { accessToken, host } = this.getters['users/getCurrentUser']
-      return new Promise((resolve, reject) => {
-        ipcRenderer.once('fetchOwnFavourite-success', (_, data) => {
-          const toots = data.result.data
-          commit('appendTimeline', {
-            timeline: toots.map(d => Toot.fromMastodon(d))
-          })
-          resolve()
-        })
-        ipcRenderer.once('fetchOwnFavourite-error', (_, e) => {
-          reject(e)
-        })
-        ipcRenderer.send('fetchOwnFavourite', {
-          host,
-          accessToken,
-          maxID
-        })
+        ipcRenderer.send('fetchOwnFavouriteTimeline', { host, accessToken, limit })
       })
     }
   }
