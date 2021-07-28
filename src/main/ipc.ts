68fbb8b5fd8f856a23c69ea97a2974cf98b00f8d
gb <13611993+netalkGB@users.noreply.github.com>
2021-07-29 01:40:13 +0900

    ipcのts化

---
+++ src/main/ipc.ts
@@ -0,0 +1,54 @@
+import { ipcMain, dialog, shell } from 'electron'
+import { Logger } from 'log4js'
+import WindowManager from '~/WindowManager'
+
+export default function (logger:Logger, windows:WindowManager)  {
+  ipcMain.on('openURL', (event, arg) => {
+    const isURL = arg.match(/^((^http|^https):\/\/)/g) !== null
+    if (isURL) {
+      shell.openExternal(arg)
+    }
+  })
+  ipcMain.handle('openDialog', () => {
+    const appendFile = dialog.showOpenDialog({
+      properties: ['openFile'],
+      filters: [
+        { name: 'Images', extensions: ['jpg', 'png', 'jpeg'] },
+        { name: 'Movies', extensions: ['mp4'] }
+      ]
+    })
+    return appendFile
+  })
+  ipcMain.on('newWindow', (_, url:string, type) => {
+    const newWindow = (args: { width: number; height: number }) => {
+      const { width, height } = args
+      windows.add(
+        url,
+        { url },
+        {
+          width,
+          height
+        })
+    }
+    switch (type) {
+      case 'newToot':
+        newWindow({ width: 320, height: 150 })
+        break
+      case 'conversations':
+        newWindow({ width: 280, height: 400 })
+        break
+      case 'imagePreview':
+        newWindow({ width: 640, height: 480 })
+        break
+      case 'searchWindow':
+        newWindow({ width: 256, height: 500 })
+        break
+      case 'favouriteWindow':
+        newWindow({ width: 256, height: 480 })
+        break
+      default:
+        newWindow({ width: 256, height: 500 })
+        break
+    }
+  })
+}
