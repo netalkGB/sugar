309baaeb77da40c8d714483fc341342875a2daf8
gb <13611993+netalkGB@users.noreply.github.com>
2021-07-29 20:06:11 +0900

    preloadのts化

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,3 +1,21 @@
+import { Logger } from 'log4js'
+import 'electron'
+interface Window {
+  NODE_ENV:string | undefined,
+  ipc:Electron.IpcRenderer
+  remote:Electron.Remote,
+  log4js:Logger,
+  uuidv4:any,
+  M:any,
+  fs:any,
+  setImmediate:any,
+  clearImmediate:any
+}
+
+declare var window:Window
+
+
+
 const _setImmediate = window.setImmediate
 const _clearImmediate = window.clearImmediate
 
