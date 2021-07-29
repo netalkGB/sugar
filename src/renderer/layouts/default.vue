f320689417371d13b6568b43d491154ffb30a05a
gb <13611993+netalkGB@users.noreply.github.com>
2021-07-30 00:29:43 +0900

    App.vueの内容を反映

---
+++ src/renderer/layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <Nuxt />
+</template>
+
+<script>
+export default {
+  name: 'default',
+  components: {}
+}
+</script>
+
+<style>
+html {
+  font-family: '游ゴシック Medium', 'Yu Gothic Medium', '游ゴシック体', YuGothic,
+    メイリオ, 'Noto Sans CJK JP', 'TakaoEXゴシック', 'TakaoExGothic', sans-serif;
+}
+html,
+body {
+  margin: 0;
+  padding: 0;
+  color: #3e3e3e;
+  user-select: none;
+  cursor: default;
+}
+*::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
+*::webkit-scrollbar-track {
+  background: #efefef;
+}
+*::-webkit-scrollbar-thumb {
+  background: #cccccc;
+}
+*::-webkit-scrollbar-thumb:hover {
+  background: #aaaaaa;
+}
+a {
+  text-decoration: none;
+  color: #5555cc;
+}
+p {
+  margin: 0;
+  padding: 0;
+}
+</style>
