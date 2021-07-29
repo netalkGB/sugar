06ecd67162366ad58ca3ae252150de1bf1e9850d
gb <13611993+netalkGB@users.noreply.github.com>
2021-07-29 20:08:33 +0900

    パッケージング用のビルドスクリプトにもpreloadをビルドする処理を追加

--- script/package.js
+++ script/package.js
@@ -1,7 +1,7 @@
 const { build } = require('electron-builder')
 const spawn = require('cross-spawn-promise')
 const config = require('../electron-builder.config.json')
-const { buildMainProcess } = require('./promiseWebpack')
+const { buildMainProcess, buildPreloadScript } = require('./promiseWebpack')
 const mode = 'production'
 
 const platform = process.argv[2]
@@ -10,6 +10,22 @@ async function main () {
   /* eslint no-console: 0 */
   try {
     const stats = await buildMainProcess({ mode })
+    console.log(stats.toString({ colors: true }))
+    if (stats.hasErrors()) {
+      process.exit(255)
+    }
+  } catch (error) {
+    console.error(error.stack || error)
+    if (err.details) {
+      for (const d of err.details) {
+        console.error('main process ' + '[err]' + ': ' + red + d + reset)
+      }
+      process.exit(255)
+    }
+  }
+
+  try {
+    const stats = await buildPreloadScript({ mode })
     console.log(stats.toString({ colors: true }))
     if (stats.hasErrors()) {
       process.exit(255)
