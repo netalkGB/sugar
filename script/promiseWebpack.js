492391846776e4baa3cfdc1d7d18658555caa4d2
gb <13611993+netalkGB@users.noreply.github.com>
2021-07-29 20:05:00 +0900

    preloadも追加

--- script/promiseWebpack.js
+++ script/promiseWebpack.js
@@ -1,10 +1,20 @@
 const webpack = require('webpack')
 const mainConfig = require('../webpack.config.main.js')
+const preloadConfig = require('../webpack.config.preload.js')
 
 module.exports = {
   buildMainProcess: ({ mode }) =>
     new Promise((resolve, reject) => {
       webpack(mainConfig({}, { mode }), (err, stats) => {
+        if (err) {
+          reject(err)
+        }
+        resolve(stats)
+      })
+    }),
+  buildPreloadScript: ({ mode }) =>
+    new Promise((resolve, reject) => {
+      webpack(preloadConfig({}, { mode }), (err, stats) => {
         if (err) {
           reject(err)
         }
