56595e7f93a53c6ad8d6e17800b30623f3675564
gb <13611993+netalkGB@users.noreply.github.com>
2021-08-01 03:38:33 +0900

    ts化に伴い追加

---
+++ src/renderer/other/TootArgs.ts
@@ -0,0 +1,30 @@
+import Profile from '@/other/Profile'
+import Media from './Media'
+
+export default interface TootArgs {
+  profile?: Profile
+  content?:string
+  date:Date
+  id:string
+  boostId?:string
+  boostsCount?:number
+  favoritesCount?:number
+  repliesCount?:number
+  visibility?: 'public' | 'unlisted' | 'private' | 'direct'
+  medium?: Array<Media>
+  favorited?: boolean | null
+  boosted?: boolean | null
+  boostedBy?: Profile
+  followedBy?: Profile
+  originalId?: string
+  isTootByOwn: boolean
+  mentions?: Array<{
+    id: string
+    acct: string
+    username: string
+    url: string
+  }>
+  warning?: boolean
+  warningComment?:string
+  favoritedBy?: Profile |undefined
+}
