f7fb84cf774347054787919ddd0a1ecacd54b3b2
gb <13611993+netalkGB@users.noreply.github.com>
2021-08-05 02:39:36 +0900

    Nuxt.jsの自動生成のルータを使用するため名前変更

---
+++ src/renderer/pages/favourite/_userId.vue
@@ -0,0 +1,64 @@
+<template>
+  <div :style="{ width: width + 'px', height: height + 'px' }">
+    <Favourite class="favourite" />
+  </div>
+</template>
+
+<script>
+import logger from '@/other/Logger'
+import Favourite from '@/components/Favourite/Favourite'
+import { createNamespacedHelpers } from 'vuex'
+const { mapActions } = createNamespacedHelpers('users')
+
+export default {
+  props: ['userId', 'screenName'],
+  components: {
+    Favourite
+  },
+  data () {
+    return {
+      width: 0,
+      height: 0
+    }
+  },
+  methods: {
+    ...mapActions(['setCurrentUserId', 'loadUserConfig'])
+  },
+  created () {
+    this.loadUserConfig()
+    this.setCurrentUserId(this.userId)
+  },
+  mounted () {
+    this.width = window.innerWidth
+    this.height = window.innerHeight
+    window.addEventListener('resize', e => {
+      this.width = window.innerWidth
+      this.height = window.innerHeight
+      logger.debug(this.width, this.height)
+    })
+    window.addEventListener('storage', event => {
+      if (event.key === 'user' + this.userId) {
+        const val = JSON.parse(event.newValue)
+        if (val.type === 'deleteToot') {
+          this.removeToot({ id: val.id })
+        }
+      }
+    })
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize', () => { })
+    window.removeEventListener('storage', () => { })
+  }
+}
+</script>
+
+<style scoped>
+.searchContainer {
+  font-size: 12px;
+  height: 100%;
+  width: 100%;
+}
+.favourite {
+  height: 100%;
+}
+</style>
