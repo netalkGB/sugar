785f2ca8837d8222c9decda3c520e8f8bb8bb9ee
gb <13611993+netalkGB@users.noreply.github.com>
2021-08-11 02:40:34 +0900

    モーダルのコンポーネント追加

---
+++ src/renderer/components/Modal/Modal.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="background" @click.self="closeModal" v-if="open">
+    <dialog class="dialog centering">
+      <div class="messages">
+        <div v-for="(message, idx) in messages" :key="idx">
+          {{ message }}
+        </div>
+      </div>
+      <div class="ok oktopmargin">
+        <MtButton @click.native="closeModal">OK</MtButton>
+      </div>
+    </dialog>
+  </div>
+</template>
+
+<script>
+import MtButton from '@/components/Form/MtButton'
+import { createNamespacedHelpers } from 'vuex'
+const { mapActions } = createNamespacedHelpers('modal')
+const { mapState } = createNamespacedHelpers('modal')
+export default {
+  components: {
+    MtButton
+  },
+  computed: {
+    ...mapState({ messages: state => state.messages, open: state => state.open })
+  },
+  methods: {
+    ...mapActions(['closeModal'])
+  }
+}
+</script>
+
+<style scoped>
+  .dialog {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    width: 200px;
+    font-size: 12px;
+    border: 2px solid #ccc;
+  }
+  .ok {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .centering {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .background {
+    position: fixed;
+    background: rgba(50,50,50, 0.5);
+    width: 100%;
+    height: 100%;
+  }
+  .oktopmargin {
+    margin-top: 8px;
+  }
+</style>
