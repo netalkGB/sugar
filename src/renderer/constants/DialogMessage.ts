70d08c25c873743d3ceb13f1b03b28b13b33ee1c
gb <13611993+netalkGB@users.noreply.github.com>
2021-08-11 22:10:39 +0900

    エラーメッセージの一覧を追加

---
+++ src/renderer/constants/DialogMessage.ts
@@ -0,0 +1,137 @@
+import Messages from '@/constants/interfaces/Messages'
+const dialogMessage:Messages = {
+  homeTimelineFetchError: [
+    {
+      language: 'ja',
+      message: 'ホームタイムラインの取得に失敗しました。'
+    }
+  ],
+  localTimelineFetchError: [
+    {
+      language: 'ja',
+      message: 'ローカルタイムラインの取得に失敗しました。'
+    }
+  ],
+  publicTimeLineFetchError: [
+    {
+      language: 'ja',
+      message: '公開タイムラインの取得に失敗しました。'
+    }
+  ],
+  notificationFetchError: [
+    {
+      language: 'ja',
+      message: '通知の取得に失敗しました。'
+    }
+  ],
+  homeTimelineStreamError: [
+    {
+      language: 'ja',
+      message: 'ホームタイムラインのストリーミングの接続でエラーが発生しました。'
+    }
+  ],
+  localTimelineStreamError: [
+    {
+      language: 'ja',
+      message: 'ローカルタイムラインのストリーミングの接続でエラーが発生しました。'
+    }
+  ],
+  publicTimeLineStreamError: [
+    {
+      language: 'ja',
+      message: '公開タイムラインのストリーミングの接続でエラーが発生しました。'
+    }
+  ],
+  conversationFetchError: [
+    {
+      language: 'ja',
+      message: '会話の取得に失敗しました。'
+    }
+  ],
+  profileFetchError: [
+    {
+      language: 'ja',
+      message: 'プロフィールの取得に失敗しました。'
+    }
+  ],
+  profileTimelineFetchError: [
+    {
+      language: 'ja',
+      message: 'プロフィールのタイムラインの取得に失敗しました。'
+    }
+  ],
+  profileFollowingFetchError: [
+    {
+      language: 'ja',
+      message: 'プロフィールのフォローの取得に失敗しました。'
+    }
+  ],
+  profileFollowersFetchError: [
+    {
+      language: 'ja',
+      message: 'プロフィールのフォロワーの取得に失敗しました。'
+    }
+  ],
+  searchResultFetchError: [
+    {
+      language: 'ja',
+      message: '検索結果の取得に失敗しました。'
+    }
+  ],
+  favoriteFetchError: [
+    {
+      language: 'ja',
+      message: 'お気に入りの取得に失敗しました。'
+    }
+  ],
+  removeTootRequestError: [
+    {
+      language: 'ja',
+      message: 'トゥートの削除に失敗しました。'
+    }
+  ],
+  boostTootRequestError: [
+    {
+      language: 'ja',
+      message: 'トゥートのブーストに失敗しました。'
+    }
+  ],
+  unBoostTootRequestError: [
+    {
+      language: 'ja',
+      message: 'トゥートのブースト解除に失敗しました。'
+    }
+  ],
+  favoriteTootRequestError: [
+    {
+      language: 'ja',
+      message: 'トゥートのお気に入り追加に失敗しました。'
+    }
+  ],
+  unFavoriteTootRequestError: [
+    {
+      language: 'ja',
+      message: 'トゥートのお気に入り解除に失敗しました。'
+    }
+  ],
+  tootRequestError: [
+    {
+      language: 'ja',
+      message: '投稿に失敗しました。'
+    }
+  ],
+  fileUploadError: [
+    {
+      language: 'ja',
+      message: 'ファイルのアップロードに失敗しました。'
+    }
+  ],
+  fileAddError: [
+    {
+      language: 'ja',
+      message: 'ファイルの追加に失敗しました。'
+    }
+  ]
+}
+
+export default dialogMessage
