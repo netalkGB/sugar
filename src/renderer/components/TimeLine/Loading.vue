51d44a516d9435062d6824f8453579afa367fe5a
gb <13611993+netalkGB@users.noreply.github.com>
2021-08-13 02:32:10 +0900

    読み込み中のコンポーネントを追加

---
+++ src/renderer/components/TimeLine/Loading.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="loading">
+    <div class="loadingicon">
+      <IosSyncIcon
+        w="40px"
+        h="40px"
+        animate="rotate"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import IosSyncIcon from 'vue-ionicons/dist/ios-sync.vue'
+export default {
+  components: {
+    IosSyncIcon
+  }
+}
+</script>
+
+<style scoped>
+.loading {
+  position: absolute;
+  top:0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+}
+.loadingicon {
+  text-align: center;
+  color: #00aaff;
+}
+</style>
